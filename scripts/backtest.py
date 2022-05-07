0b8f424e059efb9c3044a6ee7bfc12f5f7aac3f8
alexcreek <me@alexcreek.com>
2022-05-07 01:44:28 -0400

    make this purely a module

--- scripts/backtest.py
+++ scripts/backtest.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python3
-import argparse
 import os
 import sys
 from dotenv import load_dotenv
 from influxdb_client import InfluxDBClient
 from dateutil.parser import parse
 import utils
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Backtest timeseries data')
-    parser.add_argument('--start', help='day to start with', required=True)
-    parser.add_argument('-e', '--exp', help='expiration', required=True)
-    parser.add_argument('-s', '--strike', help='strike', required=True)
-    parser.add_argument('-t', '--type', help='put or call', required=True)
-    parser.add_argument('--limit', help='limit', type=float, required=True)
-    parser.add_argument('--stop', help='stop limit', type=float, required=True)
-    parser.add_argument('--time', help='time to start with', required=True)
-    parser.add_argument('-v', '--verbose', help='enable verbose logging', action='store_true')
-    return parser.parse_args()
-
 
 def main(start, exp, strike, putCall, limit, stop, time, verbose=False):
     # pylint: disable=too-many-locals
@@ -128,17 +114,3 @@ class BacktestWindow:
             float
         """
         return round((current - start) / start, 4)
-
-
-if __name__ == '__main__':
-    args = parse_arguments()
-    main(
-        args.start,
-        args.exp.upper(),
-        args.strike,
-        args.type,
-        args.limit,
-        args.stop,
-        args.time,
-        args.verbose,
-    )
