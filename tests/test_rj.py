37a1cb796823b312baf073bd224ecb79fa933513
alexcreek <me@alexcreek.com>
2022-05-12 23:16:58 -0400

    add more tests

--- tests/test_rj.py
+++ tests/test_rj.py
@@ -19,3 +19,39 @@ class TestDoesEnvvarExist:
 
     def test_a_present_envvar_does_nothing(self, monkeypatch):
         assert False
+
+class TestNotify:
+    def test_sending_a_text(self, monkeypatch):
+        assert False
+
+class TestBuy:
+    def test_something(self, monkeypatch):
+        assert False
+
+class TestFindExp:
+    def test_finding_an_exp(self, monkeypatch):
+        assert False
+
+    def test_no_exp_found(self, monkeypatch):
+        assert False
+
+class TestFetchPrice:
+    def test_success(self, monkeypatch):
+        assert False
+
+    def test_failure(self, monkeypatch):
+        assert False
+
+class TestCooldown:
+    def test_ignoring_a_buy_while_cooling_down(self, monkeypatch):
+        assert False
+
+    def test_working_like_normal(self, monkeypatch):
+        assert False
+
+class TestEvalWindow:
+    def test_calculating_change(self, monkeypatch):
+        assert False
+
+    def test_evaluating_a_time_window(self, monkeypatch):
+        assert False
