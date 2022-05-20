8cc196ac76d86f829dac778135233c4ff91bd431
alexcreek <me@alexcreek.com>
2022-05-19 23:17:31 -0400

    remove test placeholder

--- tests/test_rj.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import rj
-
-# https://docs.pytest.org/en/6.2.x/getting-started.html
-# https://docs.pytest.org/en/6.2.x/monkeypatch.html
-# https://docs.pytest.org/en/6.2.x/fixture.html
-# https://docs.pytest.org/en/6.2.x/capture.html
