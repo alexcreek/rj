71a2685a34ab66be5fcb28347a57e86282ddc109
alexcreek <me@alexcreek.com>
2022-05-22 20:38:54 -0400

    fix a typeError

--- tests/test_models.py
+++ tests/test_models.py
@@ -57,7 +57,7 @@ def poller(config, monkeypatch):
         return 400.1
     monkeypatch.setattr(spivey.Client, 'underlying', underlying)
     monkeypatch.setitem(config, 'ticker', 'SPY')
-    monkeypatch.setitem(config, 'polling_interval', '1')
+    monkeypatch.setitem(config, 'polling_interval', 1)
     return Poller(config, Queue())
 
 ### Tests
