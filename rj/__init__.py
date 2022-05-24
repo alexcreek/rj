43d6b3bbf6da84658d05d5beb7f80ee45b706127
alexcreek <me@alexcreek.com>
2022-05-23 23:43:42 -0400

    default to 80 points; price flux typically lasts 20m

--- rj/__init__.py
+++ rj/__init__.py
@@ -23,7 +23,7 @@ def configure():
             'twilio_from': os.getenv('TWILIO_FROM', '+1234567890'),
             'twilio_to': os.getenv('TWILIO_TO', '+1234567890'),
             'polling_interval': int(os.getenv('POLLING_INTERVAL', '30')),
-            'cooldown_points': int(os.getenv('COOLDOWN_POINTS', '60')),
+            'cooldown_points': int(os.getenv('COOLDOWN_POINTS', '80')),
             'live_trading': os.getenv('LIVE_TRADING', 'ENABLED').upper(),
             'client_id': os.environ['CLIENT_ID'], # tdameritrade
             'refresh_token': os.environ['REFRESH_TOKEN'], # tdameritrade
